f349c358ee6c2f2111710a846e1d3f6fe135883e
Luis Hernández <luelher@gmail.com>
2016-04-07 02:40:32 -0430

    add new search criterias

--- app/models/keyword_search.rb
+++ app/models/keyword_search.rb
@@ -7,7 +7,7 @@ class KeywordSearch
   API_VERSION = :v201509
   PAGE_SIZE = 20
 
-  ADDITIONAL_CRITERIAS = ["a", "b", "c", "d", "aa", "ab", "ac", "ad", "ba", "bb", "bc", "bd"]
+  ADDITIONAL_CRITERIAS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
   def self.get_keyword_ideas(keyword_text, country, category)
 
